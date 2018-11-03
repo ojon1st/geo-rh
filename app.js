8dae7348ac678bac2f58b7c0315b7982609a91e6
ojon1st <a.garba@hotmail.fr>
2018-11-03 23:33:21 +0100

    app.js update
    change extended url to true

--- app.js
+++ app.js
@@ -40,12 +40,13 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // use passport session
 app.use(passport.initialize());
